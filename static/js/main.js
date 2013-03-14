3eeffc5a891983fe3665582ed12360d6dc0da9c9
ZHUO Qiang <zhuo.qiang@gmail.com>
2013-03-14 10:43:06 +0800

    add moment and i18next

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,22 @@
-
+$(document).ready(function() {
+    i18n.init({
+        detectLngQS: 'lang',
+        fallbackLng: 'en',
+        lowerCaseLng: true
+    });
+    
+    var lang = i18n.lng();
+    if (moment.langData(lang)) {
+        moment.lang(lang);        
+    }
+    else {
+        lang = lang.split('-')[0]
+        moment.lang(lang);
+    }
+    
+    $('time[pubdate]').each(function() {
+        var t = $(this);
+        var time = t.attr('datetime');
+        t.html(moment(time).fromNow());
+    });
+})
