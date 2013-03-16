c0dce6be7855903e03fe41396f82e6f4bf6455b7
ZHUO Qiang <zhuo.qiang@gmail.com>
2013-03-16 22:22:26 +0800

    i18n support

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,31 @@
+var i18nResource = {
+    zh: {
+        translation: {
+            'programming': '编程',
+            'english': '英文',
+            'opensource': '开源',
+            'configuration': '配置',
+            'compiler': '编译器',
+            'concurrent': '并发',
+            'Tao can Talk': '道可叨'
+        }
+    }
+};
+
 $(document).ready(function() {
     i18n.init({
         detectLngQS: 'lang',
         fallbackLng: 'en',
-        lowerCaseLng: true
+        lowerCaseLng: true,
+        resStore: i18nResource,
+        debug: true,
+        useCookie: false,
+        fallbackLng: false
+    }).done(function() {
+        $('[i18n]').each(function() {
+            var e = $(this);
+            e.html($.t(e.html()));
+        });
     });
     
     var lang = i18n.lng();
@@ -19,7 +42,6 @@ $(document).ready(function() {
         var time = t.attr('datetime');
         t.html(moment(time).fromNow());
     });
-
 
     // $('article>footer').hide();
     
